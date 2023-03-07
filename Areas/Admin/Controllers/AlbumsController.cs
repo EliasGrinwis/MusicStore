bbb0538473ff1890231cd946459c8f4f43470d42
Elias Grinwis Plaat Stultjes <91262453+EliasGrinwis@users.noreply.github.com>
2023-03-07 08:38:11 +0100

    first commit

---
+++ Areas/Admin/Controllers/AlbumsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+
+namespace MusicStore.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = "Administrator")]
+    public class AlbumsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
